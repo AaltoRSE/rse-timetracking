5aa27a6cacd1e0a9f723cf690f27f9665794eaee
Richard Darst <rkd@zgib.net>
2021-03-30 13:15:04 +0300

    setup: Add IPython dependencies for running the notebooks
    - Perhaps not needed for all cases, can be split off later

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=[
+        'ipywidgets',
+        'matplotlib',
         'pandas',
         'plotly',
         'python-dateutil',
