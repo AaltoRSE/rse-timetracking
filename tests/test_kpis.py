dcd8badda9ae5c39fa35dc37cc7cd8118ec10c40
Richard Darst <rkd@zgib.net>
2021-03-30 14:26:17 +0300

    tests/test_kpis: Fix tests for new KPIs

--- tests/test_kpis.py
+++ tests/test_kpis.py
@@ -5,36 +5,37 @@ from rse_timetracking.kpis import parse_KPIs
 
 def test_parse_kpis():
     """Test parsing note bodies containing KPI sections"""
-    assert parse_KPIs('Time saved 2d') == ('Researcher time saved', 57600)
-    assert parse_KPIs('Time saved 1h 3m 1s') == ('Researcher time saved', 3781)
-    assert parse_KPIs('time saved: 1s') == ('Researcher time saved', 1)
-    assert parse_KPIs('tiMe saVEd: 1W  ') == ('Researcher time saved', 144000)
-    assert parse_KPIs('time saved:1m') == ('Researcher time saved', 60)
-    assert parse_KPIs('time saved1s') == ('Researcher time saved', 1)
+    assert next(parse_KPIs('/Timesaved 2d')) == ('timesaved', 57600)
+    assert next(parse_KPIs('/Timesaved 1h 3m 1s')) == ('timesaved', 3781)
+    assert next(parse_KPIs('/timesaved: 1s')) == ('timesaved', 1)
+    assert next(parse_KPIs('/tiMesaVEd: 1W  ')) == ('timesaved', 144000)
+    assert next(parse_KPIs('/timesaved:1m')) == ('timesaved', 60)
+    assert next(parse_KPIs('/timesaved1s')) == ('timesaved', 1)
     with raises(RuntimeError, match='Could not parse 1'):
-        parse_KPIs('time saved: 1')
+        next(parse_KPIs('time saved: 1'))
     with raises(RuntimeError, match='Could not parse'):
-        parse_KPIs('time savedsdfjiov2910jbjka.....#(!!@')
+        next(parse_KPIs('time savedsdfjiov2910jbjka.....#(!!@'))
 
-    assert parse_KPIs('ongoing projects 5') == ('Projects supported', 5)
-    assert parse_KPIs('ongoing projects: 500') == ('Projects supported', 500)
-    assert parse_KPIs('ongoing projects:1') == ('Projects supported', 1)
-    assert parse_KPIs('ongoing projects4') == ('Projects supported', 4)
+    assert next(parse_KPIs('/projects 5')) == ('projects', 5)
+    assert next(parse_KPIs('/projects: 500')) == ('projects', 500)
+    assert next(parse_KPIs('/projects:1')) == ('projects', 1)
+    assert next(parse_KPIs('/projects4')) == ('projects', 4)
     with raises(ValueError, match='invalid literal for int()'):
-        parse_KPIs('ongoing projects: foo')
+        next(parse_KPIs('/projects: foo'))
 
-    assert parse_KPIs('publications 5') == ('Publications supported', 5)
-    assert parse_KPIs('publications: 500') == ('Publications supported', 500)
-    assert parse_KPIs('publications:1') == ('Publications supported', 1)
-    assert parse_KPIs('publications4') == ('Publications supported', 4)
+    assert next(parse_KPIs('/publications 5')) == ('publications', 5)
+    assert next(parse_KPIs('/publications: 500')) == ('publications', 500)
+    assert next(parse_KPIs('/publications:1')) == ('publications', 1)
+    assert next(parse_KPIs('/publications4')) == ('publications', 4)
     with raises(ValueError, match='invalid literal for int()'):
-        parse_KPIs('publications: foo')
+        next(parse_KPIs('/publications: foo'))
 
-    assert parse_KPIs('open outputs 5') == ('Open outputs', 5)
-    assert parse_KPIs('open outputs: 500') == ('Open outputs', 500)
-    assert parse_KPIs('open outputs:1') == ('Open outputs', 1)
-    assert parse_KPIs('open outputs4') == ('Open outputs', 4)
+    assert next(parse_KPIs('/outputs 5')) == ('outputs', 5)
+    assert next(parse_KPIs('/outputs: 500')) == ('outputs', 500)
+    assert next(parse_KPIs('/outputs:1')) == ('outputs', 1)
+    assert next(parse_KPIs('/outputs4')) == ('outputs', 4)
     with raises(ValueError, match='invalid literal for int()'):
-        parse_KPIs('open outputs: foo')
+        next(parse_KPIs('/outputs: foo'))
 
-    assert parse_KPIs('foo bar') is None
+    with raises(StopIteration):
+        next(parse_KPIs('foo bar'))
